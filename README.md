a81e5e1556bc0a9f668d14b6381a38a071cd4e1d
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-04-03 09:45:50 -0500

    Improving Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# flask_d3_example
+Flask + D3 example
+======
+
+A simple demo that shows how to create a full stack visualization application that leverages the backend for loading only the data necessary (based on user parameters) for visualizing in the Front End.
+
+This demo uses data from data.gov (Sorry I lost the detailed link), with historical information for th US States. It shows a choropleth map of the states, with the total Revenue per state. See a demo here: http://people.ischool.berkeley.edu/~jguerra/w209/
+
+
--- index.py
+++ index.py
@@ -1,9 +1,6 @@
-from flask import Flask, url_for, render_template, jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
-import csv
 import pandas as pd
-
-
 
 
 @app.route("/")
@@ -13,13 +10,11 @@ def hello():
 @app.route("/getData/<int:year>")
 def getData(year):
     revenue = pd.read_csv("static/data/1_Revenues.csv")
-    # with open() as csvfile:
-    #   revenuesReader = csv.DictReader(csvfile)
 
     if year < 1942 or year > 2008:
         return "Error in the year range"
 
     filteredRevenue = revenue[revenue['Year4']==year][["Name","Year4", "Total Revenue","Population (000)"]]
-    # filteredRevenue = [x for x in revenuesReader ]
+
     # show the post with the given id, the id is an integer
     return filteredRevenue.to_json(orient='records')
